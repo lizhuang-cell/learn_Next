b33d98170979110f45a9e83d5e7da6a95764a4b6
lizhuang <16638770539@163.com>
2025-10-18 15:05:34 +0800

    fix: 移除Google字体，打包有问题

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,6 +1,7 @@
-import { Lusitana ,Inter } from "@next/font/google";
+// 删除字体打包有问题
+// import { Lusitana ,Inter } from "@next/font/google";
 
 // 字体设置
-export const inter = Inter({ subsets: ["latin"] });
+// export const inter = Inter({ subsets: ["latin"] });
 
-export const lusitana = Lusitana({ subsets: ["latin"] , weight: ["400", "700"]});
+// export const lusitana = Lusitana({ subsets: ["latin"] , weight: ["400", "700"]});
