9a0c80aedc4dd079900e9e97236fe1990799fff9
lizhuang <16638770539@163.com>
2025-10-18 15:12:56 +0800

    fix: 尝试解决字体问题

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,10 @@
-// 删除字体打包有问题
-// import { Lusitana ,Inter } from "@next/font/google";
+// 字体配置
+import { Lusitana, Inter } from "next/font/google";
 
 // 字体设置
-// export const inter = Inter({ subsets: ["latin"] });
+export const inter = Inter({ subsets: ["latin"] });
 
-// export const lusitana = Lusitana({ subsets: ["latin"] , weight: ["400", "700"]});
+export const lusitana = Lusitana({
+  subsets: ["latin"],
+  weight: ["400", "700"]
+});
